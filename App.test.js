6e3db7188c3a811f0d2cc8275d3fa065553152a8
beccasiegal <46900478+beccasiegal@users.noreply.github.com>
2019-06-22 16:14:23 -0400

    added smoke and snapshot tests

--- App.test.js
+++ App.test.js
@@ -7,3 +7,47 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+CardTest('Card test', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Card />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<Messages name="Messages" unread={4}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
+
+  it('renders the UI as expected with no unreads', () => {
+    const tree = renderer
+      .create(<Messages name="Messages" unread={0}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
+});
+
+ListTest('List', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<Messages name="Messages" unread={4}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
+  
+  it('renders the UI as expected with no unreads', () => {
+    const tree = renderer
+      .create(<Messages name="Messages" unread={0}/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
+});
