ea5ec1048c54285765ca08f193aa3d1c852237cd
beccasiegal <46900478+beccasiegal@users.noreply.github.com>
2019-07-03 09:14:10 -0400

    callback props updates

--- card.js
+++ card.js
@@ -6,6 +6,8 @@ export default function Card(props) {
     <div className='Card'>
       <button
         type='button'
+         onClick={() => props.onClickDelete(props.id)}
+}
       >
         delete
       </button>
@@ -13,4 +15,7 @@ export default function Card(props) {
       <p>{props.content}</p>
     </div>
   )
+}
+ Card.propTypes = {
+  onClickDelete: () => {}
 }
